bb1845222e5a3d1a802bff0cd4454308fb4435d9
Ricardo Pellecer <rpellecer@apexglobal.co>
2025-08-11 18:37:10 -0600

    test

---
+++ my_shared_db/schemas/__init__.py
@@ -0,0 +1,10 @@
+from .client import *
+from .company import *
+from .notification import *
+from .order import *
+from .order_item import *
+from .product import *
+from .requests import *
+from .user import *
+
+__all__ = []
